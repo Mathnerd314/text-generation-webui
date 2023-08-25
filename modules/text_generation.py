2db2a33469fc172ff8ec661c72b9275a218104ff
Mathnerd314 <mathnerd314.gph+hs@gmail.com>
2023-08-25 13:36:09 -0600

    actually use session

--- modules/text_generation.py
+++ modules/text_generation.py
@@ -243,6 +243,9 @@ def generate_reply_HF(question, original_question, seed, state, stopping_strings
     for k in ['max_new_tokens', 'do_sample', 'temperature', 'top_p', 'typical_p', 'repetition_penalty', 'repetition_penalty_range', 'encoder_repetition_penalty', 'top_k', 'min_length', 'no_repeat_ngram_size', 'num_beams', 'penalty_alpha', 'length_penalty', 'early_stopping', 'tfs', 'top_a', 'mirostat_mode', 'mirostat_tau', 'mirostat_eta', 'guidance_scale']:
         generate_params[k] = state[k]
 
+    if hasattr(model, 'session'):
+        generate_params['session'] = model.session
+
     if state['negative_prompt'] != '':
         generate_params['negative_prompt_ids'] = encode(state['negative_prompt'])
 
